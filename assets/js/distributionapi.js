a16b5cfde8ed81e2001ba25fe3dc3ed28788b3ea
Mary Griffus <mary.e.shultz@gmail.com>
2016-06-26 10:19:41 -0400

    changed structure of incoming data

--- assets/js/distributionapi.js
+++ assets/js/distributionapi.js
@@ -1,9 +1,10 @@
 var dataSet = [];
+var yearSet = []
 
-function callMidaas(gender, ageGroup){
+function callMidaas(ageGroup, year){
   var EPurl = "https://api.commerce.gov/midaas/distribution?";
-    EPurl += "year=2005";
-    EPurl += "&sex=" + gender;
+    EPurl += "year=" + year;
+    // EPurl += "&sex=" + gender;
     EPurl += "&agegroup=" + ageGroup;
     EPurl += "&callbackname=JSON_CALLBACK";
     EPurl += "&api_key=YvcIGwzbGiud9JcHl0gPnVJXH5B4s7b5fXOkOVuq";
@@ -12,16 +13,18 @@ function callMidaas(gender, ageGroup){
     format: "json"
   })
   .done(function(data){
-    demographic = gender + " " + ageGroup;
-    keyVal = {demographic : demographic, distribution: data}
-    dataSet.push(keyVal)
+    data["age-group"] = ageGroup;
+    dataSet[year].push(data);
   })
 }
 
 var genders = ["female", "male"];
-var ageGroups = ["18-24", "25-34", "35-44", "45-54", "55-64", "65%2B"]
+var ageGroups = ["18-24", "25-34", "35-44", "45-54", "55-64", "65%2B"];
+var years = ["2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014"];
 
-for (var i = 0; i < ageGroups.length; i ++){
-  callMidaas("female", ageGroups[i]);
-  callMidaas("male", ageGroups[i]);
+for (var j = 0; j < years.length; j ++){
+  dataSet[years[j]] = []
+  for (var i = 0; i < ageGroups.length; i ++){
+    callMidaas(ageGroups[i], years[j]);
+  }
 }
